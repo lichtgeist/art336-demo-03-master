598783aefd34ce0adfc79af2a5821e92ff779b63
lichtgeist <Samii@Samsams-MacBook-Air.local>
2014-10-09 16:36:42 -0400

    styled menu

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
 
 // initialize bigtext.js
 // Remove /* and */ to initiate the plugin
-/* $(function() {
+ $(function() {
     WebFont.load({
         custom: {
             families: ['cubano'], // font-family name
@@ -38,22 +38,22 @@
             $('#webapps-headline').bigtext();
         }
     });
-}); */
+}); 
 
 
 // initialize Menu Button Expand/Collapse
 // Remove /* and */ to initiate the plugin
-/* $(document).ready(function(){
+ $(document).ready(function(){
     $(".toggle-button").next("ul").hide();
     $(".toggle-button").click(function(){
         $(".toggle-button").next("ul").slideToggle();
     });
-});*/
+});
 
 
 // initialize responsiveslides.js
 // Remove /* and */ to initiate the plugin
-/* $(function () {
+ /* $(function () {
     $(".slider").responsiveSlides({
         auto: false,
         pager: false,
